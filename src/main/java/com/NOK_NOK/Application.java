f1f64049942a671ee05f553eed05c11f891343c2
yoon <stay_on_track@naver.com>
2025-12-04 17:53:29 +0900

    Build와 Test 환경 에러 해결 YML 환경에 맞게 작성, 에러 이유 Port가 String으로 들어가서

--- src/main/java/com/NOK_NOK/Application.java
+++ src/main/java/com/NOK_NOK/Application.java
@@ -1,5 +1,6 @@
 package com.NOK_NOK;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+        // ================================================
+        // 애플리케이션 실행 시 env 파일에서 자동으로 환경변수 셋팅
+        Dotenv env = Dotenv.configure().ignoreIfMissing().load();
+        env.entries().forEach((entry) -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+        // ================================================
+
 		SpringApplication.run(Application.class, args);
 	}
 
